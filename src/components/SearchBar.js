ab957602f1817eced4b6938b1fe4d9a7b480e197
Belen <lenchu.deanta@gmail.com>
2018-06-19 11:35:25 +0200

    Filter components visual

---
+++ src/components/SearchBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function SearchBar (props) {
+    return (
+      <input type="text" value="Type to search..." />
+    );
+}
+
+export default SearchBar;
---
+++ src/components/SortButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function SortButton (props) {
+    return (
+      <button>Sort by</button>
+    );
+}
+
+export default SortButton;
