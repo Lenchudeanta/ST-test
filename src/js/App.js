35ffaac5c05fde1fd322359e57178069edc7fcde
Belen <lenchu.deanta@gmail.com>
2018-06-20 12:21:51 +0200

    Fetch real data

--- src/js/App.js
+++ src/js/App.js
@@ -2,19 +2,45 @@ import React, { Component } from 'react';
 import FilterContainer from './containers/FilterContainer'
 import ListContainer from './containers/ListContainer'
 
+const API = "https://star-wars-api.herokuapp.com/films";
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      sortBy: "episode"
+      sortBy: "episode",
+      movies: []
     };
     this.sorting = this.sorting.bind(this);
+    this.getMovies = this.getMovies.bind(this);
   }
 
   sorting = (parameter) => {
     this.setState({
       sortBy: parameter
     });
+  }
+
+  getMovies () {
+    fetch(API).then(response => {
+      if (response.status === 200) {
+        return response.json().then(response => {
+          this.setState({
+            movies: response
+          });
+        });
+      }
+      else {
+        console.log("Bad request");
+        this.setState({
+          movies: []
+        });
+      }
+    });
+  }
+
+  componentDidMount () {
+    this.getMovies();
   }
 
   render() {
@@ -24,7 +50,10 @@ class App extends Component {
           sortBy={this.state.sortBy}
           sortMode={this.sorting}
         />
-        <ListContainer sortBy={this.state.sortBy}/>
+        <ListContainer
+          sortBy={this.state.sortBy}
+          listMovies={this.state.movies}
+        />
       </div>
     );
   }
--- src/js/containers/ListContainer.js
+++ src/js/containers/ListContainer.js
@@ -3,45 +3,6 @@ import {sortBy} from 'lodash'
 import Table from '../components/Table'
 import Description from '../components/Description'
 
-const mockedData = [
-  {
-    title: "Episode I",
-    description: "Episode I........",
-    epidode: 1,
-    year: 1999
-  },
-  {
-    title: "Episode II",
-    description: "Episode II........",
-    epidode: 2,
-    year: 2000
-  },
-  {
-    title: "Episode III",
-    description: "Episode VI........",
-    epidode: 3,
-    year: 2001
-  },
-  {
-    title: "Episode IV",
-    description: "Episode IV........",
-    epidode: 4,
-    year: 1977
-  },
-  {
-    title: "Episode V",
-    description: "Episode V........",
-    epidode: 5,
-    year: 1978
-  },
-  {
-    title: "Episode VI",
-    description: "Episode VI........",
-    epidode: 6,
-    year: 1979
-  }
-]
-
 class ListContainer extends Component {
   constructor(props) {
     super(props);
@@ -58,7 +19,7 @@ class ListContainer extends Component {
   }
 
   render() {
-    var data = sortBy(mockedData, [this.props.sortBy]);
+    var data = sortBy(this.props.listMovies, [this.props.sortBy]);
     return (
       <div className="list">
           <Table data={data} selectMovie={this.selectMovie} />
