ca3e0a82784500ed11896b996879250013038569
Belen <lenchu.deanta@gmail.com>
2018-06-20 18:30:40 +0200

    Style fixes

--- src/js/components/Row.js
+++ src/js/components/Row.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Row (props) {
     return (
       <div className="list__row" onClick={() => props.selectMovie(props.movie)}>
-        <span className="list__row-episode">EPISODE {props.movie.episode_id}</span>
+        <span className="list__row-episode">{props.movie.episode_id ? "EPISODE " + props.movie.episode_id : ""}</span>
         <span className="list__row-title">{props.movie.title} </span>
         <span className="list__row-year">{props.movie.release_date}</span>
       </div>
--- src/js/components/Table.js
+++ src/js/components/Table.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Row from './Row'
 
-const NO_RESULTS = "No found results"
+const NO_RESULTS = {
+  episode_id: null,
+  title: "Not found results",
+  release_date: ""
+}
 
 function Table (props) {
     var listItems = props.data.map((movie) => <Row key={movie.episode_id} movie={movie} selectMovie={props.selectMovie} />);
     return (
       <div className="list__table">
-        {listItems.length > 0 ? listItems : NO_RESULTS}
+        {listItems.length > 0 ? listItems : <Row key={0} movie={NO_RESULTS} selectMovie={() => {}} />}
       </div>
     );
 }
